161607558ac5d50568a47ed88755a2323a42c2ff
pav1da <pavida.jai@spumail.net>
2025-11-13 22:14:21 +0700

    delete source

--- src/components/AppHeader.jsx
+++
@@ -1,9 +0,0 @@
-const AppHeader = () => {
-  return (
-    <div className="bg-black text-white text-center py-5 border-bottom border-black">
-        <h2>CSI205 FRONT END SOFEWARE DEVELOPMENT</h2>
-    </div>
-  );
-};
-
-export default AppHeader;
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="w-full bg-blue-500 text-white text-center p-6">
-      <h1 className="text-2xl font-bold">Welcome to My React App</h1>
-    </header>
-  );
-};
-
-export default Header;
